f50c8bfab74bd8598f5f4c9536bea0593364d3b9
argodev <gorshechnikof@gmail.com>
2024-02-05 13:44:26 +0500

    add to models, admin page

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+# Use specific model - Post
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')  # Set essential fields
+    list_filter = ['status', 'created', 'publish', 'author']  # Allows set right-side menu for searching
+    search_fields = ['title', 'body']  # Add search field
+    prepopulated_fields = {'slug': ('title',)}  # Set to automatic filling of slug field in the add-post-window
+    raw_id_fields = ['author']  # Use for many-users apps, change to choice of author(search widget)
+    date_hierarchy = 'publish'  # Add to panel of navigations
+    ordering = ['status', 'publish']  # Set to sorting criteria
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    # This class need for define to status of posts
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)  # Title of post
+    slug = models.SlugField(max_length=250)  # This point for URLs
+    # Define to relationship many-to-single, every post been writen specific User
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,  # Define behavior when User was deleted, also deleting his
+                               # all posts
+                               related_name='blog_posts'
+                               )
+    body = models.TextField()  # body of post
+
+    # This code needs for define date and time, it's show status of post(created, updated, publish)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    # Limit to value of field variants from Status.choices - draft and published, and set to default status - draft
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    # This class define Meta-data of model. Show variant presentation of posts,  '-' means chronological order
+    class Meta:
+        ordering = ['-publish']
+        # This options means the index of database, needs for future migrations of database
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    # This method need for presentation name of objects
+    def __str__(self):
+        return self.title
